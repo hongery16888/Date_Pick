2eb3a89b40a53e83ebe1860ba220e856884bdbb6
Magic Hua <hongery16888@qq.com>
2015-07-29 23:13:54 +0800

    日期选择器

---
+++ app/src/main/java/iori/hd/MainActivity.java
@@ -0,0 +1,19 @@
+package iori.hd;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * ListView的加入购物车动画
+ *
+ * @author OutTime
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
